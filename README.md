f0cee71c53922d812e45373fc16d009998539f1e
Christoph Hagen <github@christophhagen.de>
2023-09-01 15:47:30 +0200

    Move files from main package

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ClairvoyantMetrics
 
-A description of this package.
+Useful extensions for using [swift-metrics](https://github.com/apple/swift-metrics) together with [Clairvoyant](https://github.com/christophhagen/Clairvoyant).
+
+The package provides a `MetricsProvider` to serve as a `MetricFactory`, so you can do:
+
+```swift
+let metrics = MetricsProvider(...)
+MetricsSystem.bootstrap(metrics)
+```
+
+For more information, see the [documentation](https://github.com/christophhagen/Clairvoyant#usage-with-swift-metrics) of the [Clairvoyant framework](https://github.com/christophhagen/Clairvoyant).
+
--- Tests/ClairvoyantMetricsTests/ClairvoyantMetricsTests.swift
+++ Tests/ClairvoyantMetricsTests/ClairvoyantMetricsTests.swift
@@ -1,11 +1,66 @@
 import XCTest
-@testable import ClairvoyantMetrics
+import Clairvoyant
+import ClairvoyantMetrics
+import Metrics
 
 final class ClairvoyantMetricsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ClairvoyantMetrics().text, "Hello, World!")
+
+    var temporaryDirectory: URL {
+        if #available(macOS 13.0, iOS 16.0, *) {
+            return URL.temporaryDirectory
+        } else {
+            // Fallback on earlier versions
+            return URL(fileURLWithPath: NSTemporaryDirectory())
+        }
+    }
+
+    var logFolder: URL {
+        temporaryDirectory.appendingPathComponent("logs")
+    }
+
+    override func setUp() async throws {
+        try removeAllFiles()
+    }
+
+    override func tearDown() async throws {
+        try removeAllFiles()
+    }
+
+    private func removeAllFiles() throws {
+        let url = logFolder
+        if FileManager.default.fileExists(atPath: url.path) {
+            try FileManager.default.removeItem(at: url)
+        }
+        MetricObserver.standard = nil
+    }
+    
+    func testBootstrap() async throws {
+        let observer = MetricObserver(
+            logFolder: logFolder,
+            logMetricId: "observer.log",
+            encoder: JSONEncoder(),
+            decoder: JSONDecoder())
+        let metrics = MetricsProvider(observer: observer)
+        MetricsSystem.bootstrap(metrics)
+
+        let counter = Counter(label: "count")
+        let result = 5
+        counter.increment(by: result)
+
+        guard let metric = observer.getMetric(id: counter.label, type: Int.self) else {
+            XCTFail("No valid metric for counter")
+            return
+        }
+
+        // Need to wait briefly here, since forwarding the log entry to the metric is done in an async context,
+        // which would otherwise happen after trying to access the log data
+        sleep(1)
+
+        let last = await metric.lastValue()
+        XCTAssertEqual(last?.value, result)
+
+        let history = await metric.fullHistory()
+        XCTAssertEqual(history.count, 1)
+        XCTAssertEqual(history.first?.value ?? 0, result)
     }
 }
